fc929bf732cc6eca34215069f10711cf9e3afe88
Sander Dahlberg <sander.dahlberg@outlook.com>
2018-12-18 06:00:14 +0100

    initial dump

---
+++ src/main/java/nl/dahlberg/demo/importer/resource/TitleBasics.java
@@ -0,0 +1,21 @@
+package nl.dahlberg.demo.importer.resource;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
+
+import java.time.Year;
+import java.util.List;
+
+@JsonPropertyOrder(value = {"tconst", "titleType", "primaryTitle", "originalTitle", "isAdult", "startYear", "endYear", "runtimeMinutes", "genres"})
+@Data
+public class TitleBasics {
+    private String tconst;
+    private TitleType titleType;
+    private String primaryTitle;
+    private String originalTitle;
+    private Boolean isAdult;
+    private Year startYear;
+    private Year endYear;
+    private int runtimeMinutes;
+    private List<Genre> genres;
+}
