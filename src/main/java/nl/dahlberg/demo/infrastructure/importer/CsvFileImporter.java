d764d70832eea54b1e7817d8ae3f3511aa4fff32
Sander Dahlberg <sander.dahlberg@outlook.com>
2018-12-19 21:38:12 +0100

    seperate csv mapper setup; not as a bean as that overwrites regular ObjectMapper bean

--- src/main/java/nl/dahlberg/demo/infrastructure/importer/CsvFileImporter.java
+++ src/main/java/nl/dahlberg/demo/infrastructure/importer/CsvFileImporter.java
@@ -1,20 +1,15 @@
 package nl.dahlberg.demo.infrastructure.importer;
 
 import com.fasterxml.jackson.databind.MappingIterator;
-import com.fasterxml.jackson.databind.deser.std.StdDelegatingDeserializer;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
+import nl.dahlberg.demo.configuration.CsvMapperProvider;
 import nl.dahlberg.demo.conversion.DomainConversionService;
 import nl.dahlberg.demo.domain.model.MovieTitle;
-import nl.dahlberg.demo.domain.repository.MovieTitleRepository;
-import nl.dahlberg.demo.infrastructure.importer.jackson.converter.BooleanConverter;
 import nl.dahlberg.demo.infrastructure.importer.model.TitleBasics;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Spliterator;
@@ -26,18 +21,10 @@ import java.util.stream.StreamSupport;
 @RequiredArgsConstructor
 public class CsvFileImporter {
     private final DomainConversionService domainConversionService;
-    private CsvMapper csvMapper;
-
-    @PostConstruct
-    public void initCsvMapper() {
-        final SimpleModule module = new SimpleModule();
-        module.addDeserializer(Boolean.class, new StdDelegatingDeserializer<>(new BooleanConverter()));
-
-        csvMapper = new CsvMapper();
-        csvMapper.registerModules(module, new JavaTimeModule());
-    }
+    private final CsvMapperProvider csvMapperProvider;
 
     public Stream<MovieTitle> importCsvStream(final InputStream inputStream) throws IOException {
+        final CsvMapper csvMapper = csvMapperProvider.getCsvMapper();
         final CsvSchema schema =
                 csvMapper.typedSchemaFor(TitleBasics.class)
                         .withHeader()
