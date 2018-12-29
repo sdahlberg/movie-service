61cf142ea834dc9be8c8cd463794142b7d6b39bc
Sander Dahlberg <sander.dahlberg@outlook.com>
2018-12-29 11:15:10 +0100

    as most converters are explicitly used added javadoc for one that is implicitly requested by Spring's request parameter binding

--- src/main/java/nl/dahlberg/movie/interfaces/converter/StringToMovieTitleTypeConverter.java
+++ src/main/java/nl/dahlberg/movie/interfaces/converter/StringToMovieTitleTypeConverter.java
@@ -4,6 +4,10 @@ import nl.dahlberg.movie.interfaces.model.MovieTitleTypeResource;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
+/**
+ * String to MovieTitleType converter. Primary use is by Spring itself: the MovieTitleTypeResource request parameter
+ * (through MovieTitleFilterCriteria) is constructed by using this converter.
+ */
 @Component
 public class StringToMovieTitleTypeConverter implements Converter<String, MovieTitleTypeResource> {
     @Override
